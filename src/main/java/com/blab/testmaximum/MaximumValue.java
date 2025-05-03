e36bd5b74755101d65c54e9e13738f412c37c5df
rohan <rohanrmanu99@gmail.com>
2025-05-03 12:31:24 +0530

    [ROHAN]create:UC1 added method to find maximum int value

--- src/main/java/com/blab/testmaximum/MaximumValue.java
+++ src/main/java/com/blab/testmaximum/MaximumValue.java
@@ -2,5 +2,13 @@ package com.blab.testmaximum;
 
 public class MaximumValue {
 
-
+    public static void checkMaxInt(Integer a,Integer b,Integer c)
+    {
+        Integer max = a;
+        if(max.compareTo(b)<0)
+            max = b;
+        if(max.compareTo(c)<0)
+            max = c;
+        System.out.println(max);
+    }
 }
